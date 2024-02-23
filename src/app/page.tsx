44f24338cc01a1b220ed03aec3955d84b63d16e8
Bang Truong <truonghaibang1997@gmail.com>
2024-02-23 23:08:41 +0800

    Landing page initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black	">
+      <div className="">
+        <h1 className="font-primary text-8xl text-white	">COMING SOON. . .</h1>
+      </div>
+    </main>
+  );
+}
